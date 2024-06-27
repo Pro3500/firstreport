9feed2f794532a2c12fec791cecb0a597b03b651
Pro3500 <mbahodirov001@gmail.com>
2024-06-27 17:22:02 +0500

    update

--- github/index.js
+++ github/index.js
@@ -1,7 +1,20 @@
 // console.log('Hello World')
 
-let name = 10
+// let name = 10
 
-let car = 5
+// let car = 5
 
-console.log(name+car);
+// console.log(name+car);
+
+
+let sum =''
+const name = ['@']
+for (let i = 0; i < name.length; i) {
+    if (name[i] === '@') {
+        console.log('@')
+    }
+    if (isNaN(name[i])) {
+        console.log([i])
+    }
+}
+console.log(sum);
