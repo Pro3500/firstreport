86699fe0f3d91db1f237b719efb91f1b7d8bc6fc
Pro3500 <mbahodirov001@gmail.com>
2024-06-29 10:11:29 +0500

    upgrade

--- github/index.js
+++ github/index.js
@@ -7,14 +7,14 @@
 // console.log(name+car);
 
 
-let sum =''
-const name = ['@']
-for (let i = 0; i < name.length; i) {
-    if (name[i] === '@') {
-        console.log('@')
-    }
-    if (isNaN(name[i])) {
-        console.log([i])
-    }
-}
-console.log(sum);
+// let sum =''
+// const name = ['@']
+// for (let i = 0; i < name.length; i) {
+//     if (name[i] === '@') {
+//         console.log('@')
+//     }
+//     if (isNaN(name[i])) {
+//         console.log([i])
+//     }
+// }
+// console.log(sum);
